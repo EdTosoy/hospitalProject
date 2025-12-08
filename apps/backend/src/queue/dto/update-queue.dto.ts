9751488eca7548fe5c1f6bbaef9efe1b99e27200
edtosoy <edberto.tosoy@gmail.com>
2025-12-09 06:00:30 +0800

    feat: add optional status field with enum validation to UpdateQueueDto

--- apps/backend/src/queue/dto/update-queue.dto.ts
+++ apps/backend/src/queue/dto/update-queue.dto.ts
@@ -1,4 +1,10 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateQueueDto } from './create-queue.dto';
+import { IsEnum, IsOptional } from 'class-validator';
+import { QueueStatus } from '@prisma/client';
 
-export class UpdateQueueDto extends PartialType(CreateQueueDto) {}
+export class UpdateQueueDto extends PartialType(CreateQueueDto) {
+  @IsOptional()
+  @IsEnum(QueueStatus)
+  status?: QueueStatus;
+}
