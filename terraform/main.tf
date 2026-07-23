bbd245a5b6963d7b47690db3762f8d2792e19365
EdTosoy <68400105+EdTosoy@users.noreply.github.com>
2026-07-24 07:33:57 +0800

    feat: terraform security group with scoped SSH and app port ingress rules

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,88 @@
 
-resource "aws_vpc" "hospital_project" {
+resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "hospital_project_vpc"
+    Name = "hospital-project-vpc"
   }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "hospital-project-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "hospital-project-igw"
+  }
+}
+
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "hospital-project-route-table"
+  }
+}
+
+resource "aws_route_table_association" "main" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
+}
+
+resource "aws_security_group" "main" {
+  name        = "hospital-project-sg"
+  description = "Security group for hospital project"
+  vpc_id      = aws_vpc.main.id
+  tags = {
+    Name = "hospital-project-sg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "main-ssh" {
+  security_group_id = aws_security_group.main.id
+
+  cidr_ipv4   = "139.135.136.168/32"
+  from_port   = 22
+  ip_protocol = "tcp"
+  to_port     = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "main-backend" {
+  security_group_id = aws_security_group.main.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 8080
+  ip_protocol = "tcp"
+  to_port     = 8080
+}
+
+resource "aws_vpc_security_group_ingress_rule" "main-frontend" {
+  security_group_id = aws_security_group.main.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 3000
+  ip_protocol = "tcp"
+  to_port     = 3000
+}
+
+resource "aws_vpc_security_group_egress_rule" "main" {
+  security_group_id = aws_security_group.main.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = "-1"
 }
