ec21983e4f8532145a2430d9bb2d8deaba8ea611
EdTosoy <68400105+EdTosoy@users.noreply.github.com>
2026-07-24 12:54:31 +0800

    fix: terraform stringlike repo

--- terraform/main.tf
+++ terraform/main.tf
@@ -167,7 +167,7 @@ resource "aws_iam_role" "github_actions" {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
           }
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:EdTosoy/hospitalProject:ref:refs/heads/devops"
+            "token.actions.githubusercontent.com:sub" = "repo:EdTosoy@*/hospitalProject@*:ref:refs/heads/devops"
           }
         }
       }
