840bf28c3741bc4d4a407d925254132623801290
EdTosoy <68400105+EdTosoy@users.noreply.github.com>
2026-07-24 12:35:32 +0800

    feat: github actions CI, build and push images to ECR via OIDC

--- terraform/main.tf
+++ terraform/main.tf
@@ -136,3 +136,75 @@ resource "aws_ecr_repository" "frontend" {
   name                 = "hospital-project-frontend"
   image_tag_mutability = "IMMUTABLE"
 }
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1c58a3a8518e8759bf075b76b750d4f2df264fcd"
+  ]
+}
+
+resource "aws_iam_role" "github_actions" {
+  name = "github-actions-ecr-push"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:EdTosoy/hospitalProject:ref:refs/heads/devops"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "github_actions_ecr" {
+  name = "ecr-push-policy"
+  role = aws_iam_role.github_actions.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:GetAuthorizationToken"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload"
+        ]
+        Resource = [
+          aws_ecr_repository.backend.arn,
+          aws_ecr_repository.frontend.arn
+        ]
+      }
+    ]
+  })
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,10 +2,14 @@ output "instance_public_ip" {
   value = aws_instance.main.public_ip
 }
 
-output "ecr_backend_url" {
+output "backend_ecr_url" {
   value = aws_ecr_repository.backend.repository_url
 }
 
-output "ecr_frontend_url" {
+output "frontend_ecr_url" {
   value = aws_ecr_repository.frontend.repository_url
+}
+
+output "github_actions_role_arn" {
+  value = aws_iam_role.github_actions.arn
 }
