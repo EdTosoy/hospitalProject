4c8614c0b9bd6f891e178546eceb48a55dd96394
EdTosoy <68400105+EdTosoy@users.noreply.github.com>
2026-07-25 14:46:57 +0800

    feat: k8s manifest for DB,BE,FE

--- terraform/main.tf
+++ terraform/main.tf
@@ -105,6 +105,16 @@ resource "aws_vpc_security_group_ingress_rule" "main-backend-nodeport" {
   to_port     = 30080
 }
 
+
+resource "aws_vpc_security_group_ingress_rule" "main-frontend-nodeport" {
+  security_group_id = aws_security_group.main.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 30081
+  ip_protocol = "tcp"
+  to_port     = 30081
+}
+
 resource "aws_vpc_security_group_egress_rule" "main" {
   security_group_id = aws_security_group.main.id
 
